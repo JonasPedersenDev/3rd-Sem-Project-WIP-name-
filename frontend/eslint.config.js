d2ae0f6a78ad0b39ef6f28ef3b7c77fb67f4af84
Allan Krog <143946881+AllankrogAAU@users.noreply.github.com>
2024-10-17 18:06:56 +0200

    Update eslint.config.js

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -8,7 +8,7 @@ export default tseslint.config(
   { ignores: ['dist'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ['*/.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -18,11 +18,20 @@ export default tseslint.config(
       'react-refresh': reactRefresh,
     },
     rules: {
+      //default react eslint rules
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      //Own eslint rules
+      'camelCase': 1,
+      'quotes' : 1,
+      'no-multiple-empty-lines': 1,
+      'react/jsx-uses-react': 2,
+      'react/jsx-no-undef': 2,
+      'react/prop-types': 2,
+
     },
   },
 )
