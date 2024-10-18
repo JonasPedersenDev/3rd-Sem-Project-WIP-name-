ae6e0d18de590ae941f2ebbc3b0f67c9d074bd4a
Allan Krog <143946881+AllankrogAAU@users.noreply.github.com>
2024-10-18 09:54:48 +0200

    Update Greet.tsx

--- frontend/src/components/Greet.tsx
+++ frontend/src/components/Greet.tsx
@@ -1,3 +1,5 @@
+//This is just a react component to test the testing environment. Use it as a template for testing react components.
+
 const Greet = ({ name }: { name?: string}) => {
     if (name) return <h1>Hello {name}</h1>;
 
