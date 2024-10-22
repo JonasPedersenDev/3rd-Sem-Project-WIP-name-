4a6c129e8f74c903ad560c2a57e5ecaf19eb1b74
Allan Krog <mq75eh@student.aau.dk>
2024-10-22 20:32:31 +0200

    ResourceGrid fake data for component check

--- frontend/src/components/TenantView/ResourceGrid/ResourceGrid.tsx
+++ frontend/src/components/TenantView/ResourceGrid/ResourceGrid.tsx
@@ -1,42 +1,36 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 import ResourceCard from "./ResourceCard";
 import AddResourceModal from "../../CaretakerView/AddResourceModal/AddResourceModal";
 
-interface Resource {
+//Fake data
+interface ResourceProps {
+  id: number;
   name: string;
   img: string;
   description: string;
   status: string;
 }
 
+const resources: ResourceProps[] = [
+  { id: 1, name: "Boremaskine", description: "En boremaskine", img: "Boremaskine.jpg", status: "available" },
+  { id: 2, name: "Hammer", description: "En hammer", img: "Hammer.jpg", status: "available" },
+  { id: 3, name: "Målebånd", description: "Et målebånd", img: "Målebånd.jpg", status: "unavailable" },
+  { id: 4, name: "Sav", description: "En sav", img: "Sav.jpg", status: "maintenance" },
+  { id: 5, name: "Skruetrækker", description: "En skruetrækker", img: "Skruetrækker.jpg", status: "available" },
+];
+
 const ResourceGrid: React.FC = () => {
-  const [resources, setResources] = useState<Resource[]>([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const fetchResources = async () => {
-    try {
-      const response = await axios.get("http://localhost:8080/api/resources");
-      setResources(response.data);
-    } catch (error) {
-      console.error("Error fetching resources:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchResources();
-  }, []);
-
   const handleResourceAdded = () => {
-    fetchResources();
     setIsModalOpen(false);
   };
 
   return (
     <div>
       <div className="row">
-        {resources.map((resource, index) => (
-          <ResourceCard key={index} resource={resource} />
+        {resources.map(resource => (
+          <ResourceCard key={resource.id} resource={resource} />
         ))}
       </div>
       <button onClick={() => setIsModalOpen(true)}>Add new resource</button>
