7e0cd0193030e9bc4604ac2ff4744dcb8c475cec
ppilbauer <144022241+ppilbauer@users.noreply.github.com>
2024-10-31 03:12:05 +0100

    Update CheckoutPage.tsx

--- frontend/src/pages/CheckoutPage.tsx
+++ frontend/src/pages/CheckoutPage.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import NavBar from "../components/BothView/NavBar/NavBar.tsx";
 import Checkout from "../components/TenantView/Checkout/Checkout.tsx";
-
+import Footer from "../components/BothView/Footer/Footer.tsx";
 
 const CheckoutPage = () => {
   return (
@@ -9,6 +9,7 @@ const CheckoutPage = () => {
       <NavBar />
       <Checkout /> <br />
       <Link to="/Hjem">Go to HomePage</Link>
+      <Footer style={footerStyle} />
     </div>
   );
 };
