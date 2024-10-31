b0993ec7a01e589cae66f1f018d6277c1a87224c
ppilbauer <144022241+ppilbauer@users.noreply.github.com>
2024-10-31 03:13:14 +0100

    Update LoginPage.tsx

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,9 +1,11 @@
 import Login from "../components/BothView/Login/Login";
+import Footer from "../components/BothView/Footer/Footer.tsx";
 
 const LoginPage = () => {
   return (
     <div>
       <Login />
+      <Footer style={footerStyle} />
     </div>
   );
 };
