7e2fa64f99ca527b98d126fd3bceb5aa896788b0
ppilbauer <144022241+ppilbauer@users.noreply.github.com>
2024-10-31 03:14:00 +0100

    Update OwnBookingsPage.tsx

--- frontend/src/pages/OwnBookingsPage.tsx
+++ frontend/src/pages/OwnBookingsPage.tsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import Footer from "../components/BothView/Footer/Footer.tsx";
 
 const OwnBookingsPage = () => {
   return (
     <div>
       <h1>OwnBookingsPage</h1>
       <Link to="/Hjem">Go to HomePage</Link>
+      <Footer style={footerStyle} />
     </div>
   );
 };
