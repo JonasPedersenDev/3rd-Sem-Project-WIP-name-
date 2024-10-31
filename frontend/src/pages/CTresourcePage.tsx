7f0de980520e70aa420559923db5cc3457e34cc2
ppilbauer <144022241+ppilbauer@users.noreply.github.com>
2024-10-31 03:11:11 +0100

    Update CTresourcePage.tsx

--- frontend/src/pages/CTresourcePage.tsx
+++ frontend/src/pages/CTresourcePage.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import NavBar from "../components/BothView/NavBar/NavBar.tsx";
+import Footer from "../components/BothView/Footer/Footer.tsx";
 
 const CTresourcePage = () => {
   return (
@@ -8,6 +9,7 @@ const CTresourcePage = () => {
     <div>
       <h1>CTresourcePage</h1>
       <Link to="/Hjem">Go to HomePage</Link>
+      <Footer style={footerStyle} />
     </div>
     </>
   );
