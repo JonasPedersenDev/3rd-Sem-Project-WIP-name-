bb7a7badf013ba41787871a5ec31ec1aad80dc44
Allan Krog <mq75eh@student.aau.dk>
2024-11-04 10:01:15 +0100

    Database updated

--- backend/src/main/java/com/auu_sw3_6/Himmerland_booking_software/api/controller/UtilityController.java
+++ backend/src/main/java/com/auu_sw3_6/Himmerland_booking_software/api/controller/UtilityController.java
@@ -4,11 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.auu_sw3_6.Himmerland_booking_software.api.model.Utility;
 import com.auu_sw3_6.Himmerland_booking_software.service.UtilityService;
 
 @RestController
 @RequestMapping("api/utility")
-public class UtilityController extends ResourceController {
+public class UtilityController extends ResourceController<Utility> {
 
     @Autowired
     public UtilityController(UtilityService UtilityService) {
