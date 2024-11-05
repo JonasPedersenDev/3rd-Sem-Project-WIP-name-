de4e40622ddcc19534650e485972398655a59a79
Allan Krog <mq75eh@student.aau.dk>
2024-11-05 12:37:26 +0100

    Jonas pls fix

--- backend/src/main/java/com/auu_sw3_6/Himmerland_booking_software/api/controller/ToolController.java
+++ backend/src/main/java/com/auu_sw3_6/Himmerland_booking_software/api/controller/ToolController.java
@@ -1,6 +1,10 @@
 package com.auu_sw3_6.Himmerland_booking_software.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +15,16 @@ import com.auu_sw3_6.Himmerland_booking_software.service.ToolService;
 @RequestMapping("api/tool")
 public class ToolController extends ResourceController<Tool> {
 
+    private final ToolService toolService;
+
     @Autowired
-    public ToolController(ToolService ToolService) {
-        super(ToolService);
+    public ToolController(ToolService toolService) {
+        super(toolService);
+        this.toolService = toolService;
     }
 
+    @GetMapping(value = "/all", produces = "application/json")
+    public ResponseEntity <List<Tool>> getAllTools() {
+        return ResponseEntity.ok(toolService.getAllResources());
+    }
 }
