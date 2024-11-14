87106314f226bc3520a68ad475a55bc77dd38175
JonasPedersen123 <jopede23@student.aau.dk>
2024-11-14 12:31:36 +0100

    Fixed endpoint problem

--- backend/src/main/java/com/auu_sw3_6/Himmerland_booking_software/api/controller/ResourceController.java
+++ backend/src/main/java/com/auu_sw3_6/Himmerland_booking_software/api/controller/ResourceController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,6 @@ import com.auu_sw3_6.Himmerland_booking_software.service.BookingService;
 import com.auu_sw3_6.Himmerland_booking_software.service.ResourceService;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("api/resource")
