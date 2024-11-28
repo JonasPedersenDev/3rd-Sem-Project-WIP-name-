0a5800412f5fbeab0faaca47376932f72d4b74cf
JonasPedersen123 <jopede23@student.aau.dk>
2024-11-28 23:41:58 +0100

    Just something to commit

--- backend/src/main/java/com/auu_sw3_6/Himmerland_booking_software/config/security/CustomUserDetails.java
+++ backend/src/main/java/com/auu_sw3_6/Himmerland_booking_software/config/security/CustomUserDetails.java
@@ -10,52 +10,52 @@ import com.auu_sw3_6.Himmerland_booking_software.api.model.User;
 
 public class CustomUserDetails extends User implements UserDetails {
 
-    private final User user;
-    private final List<GrantedAuthority> authorities;
+  private final User user;
+  private final List<GrantedAuthority> authorities;
 
-    public CustomUserDetails(User user, List<GrantedAuthority> authorities) {
-        this.user = user;
-        this.authorities = authorities;
-    }
+  public CustomUserDetails(User user, List<GrantedAuthority> authorities) {
+    this.user = user;
+    this.authorities = authorities;
+  }
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return authorities;
+  }
 
-    @Override
-    public String getPassword() {
-        return user.getPassword();
-    }
+  @Override
+  public String getPassword() {
+    return user.getPassword();
+  }
 
-    @Override
-    public String getUsername() {
-        return user.getUsername();
-    }
+  @Override
+  public String getUsername() {
+    return user.getUsername();
+  }
 
-    public User toUser() {
-        return user;
-    }
+  public User toUser() {
+    return user;
+  }
 
-    // We maybe should impliment these methods
+  // We maybe should impliment these methods
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
 
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
 
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
 
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }
